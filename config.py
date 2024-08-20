b21dce15edd439ec20ebea07b5d47e54d3287435
Stepan <kornevstepan169@gmail.com>
2024-08-20 15:45:12 +0700

    Add check for required .env variables

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ load_dotenv()
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 session_name = os.getenv('SESSION_NAME')
+
+if not all([api_id, api_hash, session_name]):
+    raise ValueError('Необходимо указать API_ID, API_HASH и SESSION_NAME в файле .env')
